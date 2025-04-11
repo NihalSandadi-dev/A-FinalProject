2231d01b43966af3709f0e5b7ff837a0b641911f
Aryan Kakadia <aryan.kakadia10@gmail.com>
2025-04-11 15:04:41 -0700

    Add helper class

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,13 @@
 from math import sqrt
+import heapq
 
+class PriorityNode:
+    def __init__(self, key, priority):
+        self.key = key
+        self.priority = priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
 
 def calculate_euclidean_disatance(start, end):
     x1, y1 = start
